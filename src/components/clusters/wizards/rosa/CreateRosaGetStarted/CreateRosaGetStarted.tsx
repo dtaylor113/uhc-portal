bfeefb60403f72e884b924df6baa48bbd3ecae33
Lior Keren <lkeren@redhat.com>
2025-08-07 19:09:50 +0300

    feat(CreateRosaGetStarted): Switch between the Rosa Architecture Alert Banner and the description found at the top of the page

--- src/components/clusters/wizards/rosa/CreateRosaGetStarted/CreateRosaGetStarted.tsx
+++ src/components/clusters/wizards/rosa/CreateRosaGetStarted/CreateRosaGetStarted.tsx
@@ -55,7 +55,6 @@ const breadcrumbs = (
 const CreateRosaGetStarted = () => (
   <AppPage>
     <PageTitle breadcrumbs={breadcrumbs} title={title(productName)}>
-      <RosaArchitectureRenamingAlert />
       <Content className="pf-v6-u-mt-md pf-v6-u-mb-md">
         <Content component={ContentVariants.p}>
           Deploy fully operational and managed Red Hat OpenShift clusters while leveraging the full
@@ -66,6 +65,7 @@ const CreateRosaGetStarted = () => (
           <ExternalLink href={links.ROSA_COMMUNITY_SLACK}>Slack us</ExternalLink>
         </Content>
       </Content>
+      <RosaArchitectureRenamingAlert />
       <Alert
         variant={AlertVariant.info}
         isInline
